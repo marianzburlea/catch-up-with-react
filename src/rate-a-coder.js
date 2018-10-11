6ddcb06f2dd06794e03aa421e13162ed9dace3a1
Marian Zburlea <marianzburlea@gmail.com>
2018-10-11 16:41:50 +0100

    update the Coder Component to be able to receive some data

--- src/rate-a-coder.js
+++ src/rate-a-coder.js
@@ -2,17 +2,29 @@
 const RateACoder = () => {
   return React.createElement('div', {}, 
     React.createElement('h1', {}, 'Rate a Coder'),
-    React.createElement(Coder),
-    React.createElement(Coder),
-    React.createElement(Coder),
+    React.createElement(Coder, {
+      name: 'Marian',
+      job: 'Front-End',
+      about: 'I make the front-end world a better place every day'
+    }),
+    React.createElement(Coder, {
+      name: 'Gratiela',
+      job: 'Social Marketing',
+      about: 'I make everyone happier every day'
+    }),
+    React.createElement(Coder, {
+      name: 'Maya',
+      job: 'Cute girl',
+      about: 'I make the my parents happy every day'
+    }),
   );
 }
 
-const Coder = () => {
+const Coder = data => {
   return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Marian'),
-    React.createElement('h2', {}, 'Front-End'),
-    React.createElement('p', {}, 'I make the front-end world a better place every day')
+    React.createElement('h1', {}, data.name),
+    React.createElement('h2', {}, data.job),
+    React.createElement('p', {}, data.about)
   ]);
 }
 
