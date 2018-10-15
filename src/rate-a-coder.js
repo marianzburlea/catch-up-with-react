9b404ff974c1eb37cb10513a0a19f477f18f1e4a
Marian Zburlea <marianzburlea@gmail.com>
2018-10-15 12:17:52 +0100

    add a click event :)

--- src/rate-a-coder.js
+++ src/rate-a-coder.js
@@ -1,23 +1,29 @@
 // add some JavaScript
 class RateACoder extends React.Component {
+
+  logClick(e) {
+    console.log('You have clicked: ' + e.target.textContent);
+  }
+
   render() {
-    return React.createElement('div', {}, 
-      React.createElement('h1', {}, 'Rate a Coder'),
-      React.createElement(Coder, {
-        name: 'Marian',
-        job: 'Front-End',
-        about: 'I make the front-end world a better place every day'
-      }),
-      React.createElement(Coder, {
-        name: 'Gratiela',
-        job: 'Social Marketing',
-        about: 'I make everyone happier every day'
-      }),
-      React.createElement(Coder, {
-        name: 'Maya',
-        job: 'Cute girl',
-        about: 'I make the my parents happy every day'
-      }),
+    return React.createElement('div', {}, [
+        React.createElement('h1', {onClick: this.logClick}, 'Rate a Coder'),
+        React.createElement(Coder, {
+          name: 'Marian',
+          job: 'Front-End',
+          about: 'I make the front-end world a better place every day'
+        }),
+        React.createElement(Coder, {
+          name: 'Gratiela',
+          job: 'Social Marketing',
+          about: 'I make everyone happier every day'
+        }),
+        React.createElement(Coder, {
+          name: 'Maya',
+          job: 'Cute girl',
+          about: 'I make the my parents happy every day'
+        })
+      ]
     );
   }
 }
