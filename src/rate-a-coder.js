72883bf6b206a3fc86e416073a39f3f17468dbe0
Marian Zburlea <marianzburlea@gmail.com>
2018-10-15 11:59:19 +0100

    refactor function component into class component

--- src/rate-a-coder.js
+++ src/rate-a-coder.js
@@ -1,23 +1,25 @@
 // add some JavaScript
-const RateACoder = () => {
-  return React.createElement('div', {}, 
-    React.createElement('h1', {}, 'Rate a Coder'),
-    React.createElement(Coder, {
-      name: 'Marian',
-      job: 'Front-End',
-      about: 'I make the front-end world a better place every day'
-    }),
-    React.createElement(Coder, {
-      name: 'Gratiela',
-      job: 'Social Marketing',
-      about: 'I make everyone happier every day'
-    }),
-    React.createElement(Coder, {
-      name: 'Maya',
-      job: 'Cute girl',
-      about: 'I make the my parents happy every day'
-    }),
-  );
+class RateACoder extends React.Component {
+  render() {
+    return React.createElement('div', {}, 
+      React.createElement('h1', {}, 'Rate a Coder'),
+      React.createElement(Coder, {
+        name: 'Marian',
+        job: 'Front-End',
+        about: 'I make the front-end world a better place every day'
+      }),
+      React.createElement(Coder, {
+        name: 'Gratiela',
+        job: 'Social Marketing',
+        about: 'I make everyone happier every day'
+      }),
+      React.createElement(Coder, {
+        name: 'Maya',
+        job: 'Cute girl',
+        about: 'I make the my parents happy every day'
+      }),
+    );
+  }
 }
 
 const Coder = data => {
